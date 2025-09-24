8a922683cf8a6989ec5c1c5193c3132cd450785d
Denis Tokmenko <98480275+ATWA66@users.noreply.github.com>
2025-09-24 15:09:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-BOT_NUMBER = '+90993123123'
+BOT_NUMBER = '+1111111111'
 ADMIN_ACCOUNT = 'YourAdminAcc@domain.net'
 FILE_NAME = "user.csv"
 DOMAIN = "domain.net"
 ORG_UNIT_PATH = "/"
 SERVICE_ACCOUNT_FILE = './service_account_key.json'
+
 SCOPES = ['https://www.googleapis.com/auth/admin.directory.user']
